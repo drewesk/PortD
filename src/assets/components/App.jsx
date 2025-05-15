ebc2dfe9c9df19901e3e2918736149768363d11e
username <drewesk@gmail.com>
2025-05-14 20:07:17 -0400

    playing with Three.js and commenting out code

--- src/assets/components/App.jsx
+++ src/assets/components/App.jsx
@@ -1,37 +1,37 @@
-import { useState } from 'react'
-import viteLogo from '../drew.png'
-import './App.css'
-import {Footer} from './Footer'
-import {Intro} from './Intro'
+// import { useState } from 'react'
+// import viteLogo from '../drew.png'
+// import './App.css'
+// import {Footer} from './Footer'
+// import {Intro} from './Intro'
 
-export const App = () => {
-  const [count, setCount] = useState(0)
+// export const App = () => {
+//   const [count, setCount] = useState(0)
 
-  return (
-    <>
-    <Intro/>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={viteLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Footer/>
-    </>
-  )
-}
+//   return (
+//     <>
+//     <Intro/>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={viteLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//       <Footer/>
+//     </>
+//   )
+// }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import { App } from "./assets/components/App";
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
 import './index.css'
-import { App } from "./assets/components/App";
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const temp = {
+  width: 1024,
+  height: 720
+}
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  temp.width / temp.height
 )
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(temp.width, temp.height)
+document.body.appendChild(renderer.domElement)
+
+camera.position.z = 4
+
+renderer.render(scene, camera)
